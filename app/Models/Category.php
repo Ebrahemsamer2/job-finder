49f94625bcd150acb1d4f1412e91853b6ae7f8aa
Ebrahem Samer <ebrahemsamer2@gmail.com>
2024-01-25 17:00:50 +0200

    Listing top Categories in Home page

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Facades\DB;
+
 class Category extends Model
 {
     use HasFactory;
 
+    public static function topCategories(int $limit)
+    {
+        return self::select('categories.id', 'categories.name', DB::raw('COUNT(jobs.id) as jobs_count'))
+        ->join('jobs', 'categories.id', '=', 'jobs.category_id')
+        ->groupBy('categories.id')
+        ->orderBy('jobs_count', 'DESC')
+        ->take($limit)
+        ->get();
+    }
+
+    // Relations
     public function jobs()
     {
         return $this->hasMany(Job::class);
